6b5feef7e0c3e259a3c4b1160f3c3eff86094401
irisoren-sg <iris.oren@gov.scot>
2021-09-01 13:44:23 +0100

    create syscost

---
+++ R/summary_functions.R
@@ -0,0 +1,22 @@
+
+#' @export
+syscost <- function(dat){
+  if("reg_obj" %in% dat$attribute == F){
+    stop("Check data. Missing attribute `reg_obj'")
+  }
+  if("region" %in% names(dat) == F){
+    stop("Check data. Missing column: `region`")
+  }
+
+  out <- dat %>%
+    filter(attribute == "reg_obj")
+
+  if(nrow(out) != unique(dat$region)){
+    stop("Data issue: The number of regions does not equal
+         the reg_obj attributes.")
+  }
+  out <- out
+    group_by(region) %>%
+    summarise(syscost = pv)
+
+}
